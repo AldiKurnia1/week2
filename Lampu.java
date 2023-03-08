3a00754e17488ee079244186963732fbc7540ac0
AldiKurnia1 <115597697+AldiKurnia1@users.noreply.github.com>
2023-03-08 21:34:29 +0700

    Add files via upload

---
+++ Lampu.java
@@ -0,0 +1,15 @@
+public class Lampu {
+    boolean nyala;
+
+    public static void nyalakanlampu(){
+        boolean nyala = true;
+        System.out.println("Apakah Lampu Menyala? "+ nyala);
+    }
+
+    public static void matikanlampu(){
+        boolean nyala = false;
+        System.out.println("Apakah Lampu Menyala? "+ nyala);
+
+    }
+    
+}
