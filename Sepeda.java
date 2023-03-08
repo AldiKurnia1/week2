62e4c8c1948a4f432fe622895fae38a4369fef8c
AldiKurnia1 <115597697+AldiKurnia1@users.noreply.github.com>
2023-03-08 21:32:29 +0700

    Add files via upload

---
+++ Sepeda.java
@@ -0,0 +1,13 @@
+public class Sepeda{
+    public static void Sepeda(int jumlahRoda, String jenis, String merk){
+        System.out.println("Sepeda "+jenis+" bermerk "+merk+" memiliki jumlah roda "+jumlahRoda);
+    }
+    
+    int gear = 5;
+
+    public static void ngerem(){
+        System.out.println("sepeda direm");
+    }
+    
+}
+
