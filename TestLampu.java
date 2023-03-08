79ef1f4a3f0c45e2d265286c526e39fc89fdba37
AldiKurnia1 <115597697+AldiKurnia1@users.noreply.github.com>
2023-03-08 21:33:27 +0700

    Add files via upload

---
+++ TestLampu.java
@@ -0,0 +1,12 @@
+public class TestLampu {
+    public static void main(String[] args){
+        Lampu Senter = new Lampu();
+
+        boolean nyala = Senter.nyala;
+        
+        Senter.nyalakanlampu();
+        Senter.matikanlampu();
+        
+    }
+    
+}
